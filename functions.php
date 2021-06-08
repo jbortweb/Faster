7c8b8756836efeb84861e8515928ed4f58b54efe
jbortweb <jbortweb@gmail.com>
2021-06-08 11:40:24 +0200

    si

--- functions.php
+++ functions.php
@@ -17,8 +17,13 @@ if(!function_exists('fwpt_scripts')):
 
         wp_register_style('style', get_stylesheet_uri(), array('google-fonts'),'1.0.0', 'all');
 
-        wp_enqueue_style('google-fonts');
+        wp_register_script('scripts', get_template_directory_uri().'/script.js',array('jquery'),'1.0.0',true); /* Nombre que le damos al script, ubicacion y nombre del archivo, jquery, version, true para que se ejecute en el footer y no en el head*/
+
+        wp_enqueue_style('google-fonts'); /*Nombre con el que registramos la accion*/
         wp_enqueue_style('style');
+        wp_enqueue_script('scripts'); 
+        wp_enqueue_script('jquery');
+
     }
     
 endif;
