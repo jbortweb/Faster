89db42ba7336cac44e00bf51930af001abb02892
jbortweb <jbortweb@gmail.com>
2021-06-08 12:17:05 +0200

    si

--- content.php
+++ content.php
@@ -1,7 +1,17 @@
 <article class="Content">
 
     <h1>Creación de Temas</h1>
-    <h2>Contenido</h2>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis laboriosam soluta impedit id totam? Aliquam, quae veniam aspernatur, adipisci ex sunt, dolor ratione saepe nostrum reiciendis odio. Blanditiis, consectetur sunt?</p>
+
+        <h2>Contenido</h2>
+
+            <?php if(have_posts()):while(have_posts()):the_post();?> <!--Si hay contenido en entradas, cuando haya contenido, publicar contenido-->
+
+                <p>Contenido de la entrada</p>
+
+            <?php endwhile;else:?>
+
+                <p>El contenido solicitado no existe</p>
+
+            <?php endif;?>
 
 </article>
