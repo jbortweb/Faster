0c1a1be864c4d86a7d93b9e280fb4435114eca3b
jbortweb <jbortweb@gmail.com>
2021-08-10 12:20:30 +0200

    añadir widget footer

--- footer.php
+++ footer.php
@@ -10,6 +10,16 @@ wp_nav_menu(array(
     'container_class'=>'SocialMedia' //Nombre de la clase que le ponemos
 ));
 ;?>
+<?php 
+    if (is_active_sidebar('footer_sidebar')):
+    dynamic_sidebar('footer_sidebar');
+    else:
+    ?>
+    <li class="Widget">
+        <h3><?php _e('Buscar','fwpt');?></h3>
+        <?php get_search_form();?>
+    </li>
+    <?php endif;?>
 
     <div>
         <small>&copy,<?php echo date('Y');?> by tuwebentrelineas.es</small>
